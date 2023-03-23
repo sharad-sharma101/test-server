db5be59163ea61f4107a96eb0a860be11032b675
Sharad Sharma <106671710+sharad-sharma101@users.noreply.github.com>
2023-03-23 20:28:34 +0530

    update insta mojo api

--- index.js
+++ index.js
@@ -1,57 +1,81 @@
 const express = require("express")
 const invoiceRouter = require("./routers/invoices");
-const bodyParser = require('body-parser')
-require("dotenv").config()
 const mongoose=require('mongoose')
 const app = express();
-
+const url = require('url');
 var cors = require('cors')
-
+const User = require('./model/Invoice')
 app.use(cors()) // Use this after the variable declaration
 
+// Credential for payment instamojo
 const Insta = require("instamojo-nodejs")
-//app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.urlencoded({extended:false}))
 const API_Key = "test_f7b3a36d1bbff9e3a853159c46a";
 const Auth_Token = "test_c6885f7b96b5e4e3adc15df807f";
 Insta.setKeys(API_Key, Auth_Token);
-Insta.isSandboxMode(true);
+
 
 app.use(express.json());
 const mongoose_URL= "mongodb+srv://sharadsharma-101:1mA66JmK1Ub2o660@cluster0.80uxmh1.mongodb.net/?retryWrites=true&w=majority";
 
+// to check connection 
 app.get('/', (req, res) => {
     res.sendStatus(200)
   })
 
-
 app.use("/api/invoice",invoiceRouter )
 
+
+
+// router for payment of invoice 
 app.post('/pay', (req,res)=>{
-    //console.log(req.body.name);
-   const data = new Insta.PaymentData();
-   const Redirect_url = "https://instamojo-test-server.onrender.com/success"
-   data.setRedirectUrl(Redirect_url);
-   data.send_email = "True";
-   data.purpose = "Test"
-   data.amount = req.body.amount;
-   data.name = req.body.name;
-   data.gmail = req.body.gmail;
 
-   Insta.createPayment(data, function(error , response){
-    if(error){
-        console.log(error);
-    } else {
-        res.send("Please check your email");
-    }
-   })
+    const data = new Insta.PaymentData();
+	Insta.isSandboxMode(true);
+	data.purpose = "For test purpose";
+	data.amount = req.body.amount;
+	data.buyer_name =  req.body.name;
+	data.email =  req.body.gmail;
+	data.redirect_url = req.body.url
+	data.phone =  "9999999999"
+	data.send_email = false;
+	data.webhook = "http://www.example.com/webhook"
+	data.send_sms = false;
+
+	Insta.createPayment(data, function(error, response) {
+		if (error) {
+			console.log(error);
+		} else {
+
+			// Payment redirection link at response.payment_request.longurl
+			const responseData = JSON.parse( response );
+			const redirectUrl = responseData.payment_request.longurl;
+			console.log( redirectUrl);
+			res.status( 200 ).json( redirectUrl );
+		}
+	});
+
 })
 
-app.get("/success" , (req,res)=>{
-    res.send("payment is success")
+// router for callback payment 
+app.get('/callback', (req,res) =>{
+   let url_parts = url.parse(req.url , true);
+   const responseData = url_parts.query;
+
+   // if payment id genrated payment is successfull 
+   if( responseData.payment_id){
+	let userID = responseData.user_id;
+	User.findByIdAndUpdate( {_id: userID}, {status: true}, {new: true})
+	 .then( (user) => res.json(user))
+	 .catch( (err) => console.log(err))
+    
+	// redirect to dashboard after successfull payment
+	return res.redirect('https://instamojo-invoice.onrender.com')
+   }
+
 })
 
-
+// connection with mongoose 
 mongoose.connect(mongoose_URL, ()=>{
     try{
     console.log("connect to mongoose")
@@ -60,11 +84,10 @@ mongoose.connect(mongoose_URL, ()=>{
     }
 })
 
+
 app.listen(4000 , ()=>{
     console.log("working fine");
 })
-
-
 
 
 /// 1mA66JmK1Ub2o660
